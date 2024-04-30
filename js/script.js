50ecb5b5f4291734e8b04135e484c106cf0a152a
Swaleh Hussein <hus.swaleh@yahoo.com>
2024-04-30 23:56:06 +0100

    add hero-page image

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// Set current year
+var yearEl = document.querySelector(".year");
+yearEl.textContent = new Date().getFullYear();
+
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) {
+    document.body.classList.add("no-flexbox-gap");
+  }
+}
+
+checkFlexGap();
