4a4e78a1ae80d08274eef8db1b8abff9965bc343
Swaleh Hussein <hus.swaleh@yahoo.com>
2024-05-01 08:32:00 +0100

    complete sticky navigation

--- js/script.js
+++ js/script.js
@@ -32,7 +32,38 @@ allLinks.forEach((link) => {
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
   });
+
+  if (link.classList.contains("main-nav-link")) {
+    link.addEventListener("click", () => {
+      headerEl.classList.toggle("nav-open");
+    });
+  }
 });
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
